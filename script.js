10f8501b3a4ff97642d50c5c175d1b66dbeb6ef3
leo <leocherin@gmail.com>
2024-07-25 13:30:46 +0100

    added skeleton quick add

--- script.js
+++ script.js
@@ -12,7 +12,80 @@ c.height = c.clientHeight;
 c.ondrop = drop;
 c.ondragover = allowDrop;
 // Create a button for every glyph in the system
-var files = ['aptamer.svg', 'assembly-scar.svg', 'association.svg', 'blunt-restriction-site.svg', 'cds-arrow.svg', 'cds.svg', 'cds_blue.svg', 'cds_green.svg', 'cds_pink.svg', 'cds_red.svg', 'cds_yellow.svg', 'chromosomal-locus.svg', 'circular-plasmid.svg', 'complex-sbgn.svg', 'composite.svg', 'control.svg', 'degradation.svg', 'dissociation.svg', 'dna-stability-element.svg', 'dsNA.svg', 'engineered-region.svg', 'five-prime-overhang.svg', 'five-prime-sticky-restriction-site.svg', 'generic-sbgn.svg', 'halfround-rectangle.svg', 'inert-dna-spacer.svg', 'inhibition.svg', 'insulator.svg', 'intron.svg', 'location-dna-no-top.svg', 'location-dna.svg', 'location-protein-no-top.svg', 'location-protein.svg', 'location-rna-no-top.svg', 'location-rna.svg', 'macromolecule.svg', 'na-sbgn.svg', 'ncrna.svg', 'no-glyph-assigned.svg', 'nuclease-site.svg', 'omitted-detail.svg', 'operator.svg', 'origin-of-replication.svg', 'origin-of-transfer.svg', 'polyA.svg', 'polypeptide-region.svg', 'primer-binding-site.svg', 'process.svg', 'promoter.svg', 'protease-site.svg', 'protein-stability-element.svg', 'protein.svg', 'replacement-glyph.svg', 'ribonuclease-site.svg', 'ribosome-entry-site.svg', 'rna-stability-element.svg', 'signature.svg', 'simple-chemical-circle.svg', 'simple-chemical-hexagon.svg', 'simple-chemical-pentagon.svg', 'simple-chemical-triangle.svg', 'specific-recombination-site.svg', 'ssNA.svg', 'stimulation.svg', 'terminator.svg', 'three-prime-overhang.svg', 'three-prime-sticky-restriction-site.svg', 'transcription-end.svg', 'translation-end.svg', 'unspecified-glyph.svg'];
+const files = ['aptamer.svg', 'assembly-scar.svg', 'association.svg', 'blunt-restriction-site.svg', 'cds-arrow.svg', 'cds.svg', 'cds_blue.svg', 'cds_green.svg', 'cds_pink.svg', 'cds_red.svg', 'cds_yellow.svg', 'chromosomal-locus.svg', 'circular-plasmid.svg', 'complex-sbgn.svg', 'composite.svg', 'control.svg', 'degradation.svg', 'dissociation.svg', 'dna-stability-element.svg', 'dsNA.svg', 'engineered-region.svg', 'five-prime-overhang.svg', 'five-prime-sticky-restriction-site.svg', 'generic-sbgn.svg', 'halfround-rectangle.svg', 'inert-dna-spacer.svg', 'inhibition.svg', 'insulator.svg', 'intron.svg', 'location-dna-no-top.svg', 'location-dna.svg', 'location-protein-no-top.svg', 'location-protein.svg', 'location-rna-no-top.svg', 'location-rna.svg', 'macromolecule.svg', 'na-sbgn.svg', 'ncrna.svg', 'no-glyph-assigned.svg', 'nuclease-site.svg', 'omitted-detail.svg', 'operator.svg', 'origin-of-replication.svg', 'origin-of-transfer.svg', 'polyA.svg', 'polypeptide-region.svg', 'primer-binding-site.svg', 'process.svg', 'promoter.svg', 'protease-site.svg', 'protein-stability-element.svg', 'protein.svg', 'replacement-glyph.svg', 'ribonuclease-site.svg', 'ribosome-entry-site.svg', 'rna-stability-element.svg', 'signature.svg', 'simple-chemical-circle.svg', 'simple-chemical-hexagon.svg', 'simple-chemical-pentagon.svg', 'simple-chemical-triangle.svg', 'specific-recombination-site.svg', 'ssNA.svg', 'stimulation.svg', 'terminator.svg', 'three-prime-overhang.svg', 'three-prime-sticky-restriction-site.svg', 'transcription-end.svg', 'translation-end.svg', 'unspecified-glyph.svg'];
+
+const hashtable = new Map([
+['aptamer.svg', ['rbcl']],
+['assembly-scar.svg', ['rbcl']],
+['association.svg',['rbcl']],
+['blunt-restriction-site.svg',['rbcl']],
+['cds-arrow.svg',['rbcl']],
+['cds.svg',['rbcl']],
+['cds_blue.svg',['rbcl']],
+['cds_green.svg',['rbcl']],
+['cds_pink.svg',['rbcl']],
+['cds_red.svg',['rbcl']],
+['cds_yellow.svg',['rbcl']],
+['chromosomal-locus.svg',['rbcl']],
+['circular-plasmid.svg',['rbcl']],
+['complex-sbgn.svg',['rbcl']],
+['composite.svg',['rbcl']],
+['control.svg',['rbcl']],
+['degradation.svg',['rbcl']],
+['dissociation.svg',['rbcl']],
+['dna-stability-element.svg',['rbcl']],
+['dsNA.svg',['rbcl']],
+['engineered-region.svg',['rbcl']],
+['five-prime-overhang.svg',['rbcl']],
+['five-prime-sticky-restriction-site.svg',['rbcl']],
+['generic-sbgn.svg',['rbcl']],
+['halfround-rectangle.svg',['rbcl']],
+['inert-dna-spacer.svg',['rbcl']],
+['inhibition.svg',['rbcl']],
+['insulator.svg',['rbcl']],
+['intron.svg',['rbcl']],
+['location-dna-no-top.svg',['rbcl']],
+['location-dna.svg',['rbcl']],
+['location-protein-no-top.svg',['rbcl']],
+['location-protein.svg',['rbcl']],
+['location-rna-no-top.svg',['rbcl']],
+['location-rna.svg',['rbcl']],
+['macromolecule.svg',['rbcl']],
+['na-sbgn.svg',['rbcl']],
+['ncrna.svg',['rbcl']],
+['no-glyph-assigned.svg',['rbcl']],
+['nuclease-site.svg',['rbcl']],
+['omitted-detail.svg',['rbcl']],
+['operator.svg',['rbcl']],
+['origin-of-replication.svg',['rbcl']],
+['origin-of-transfer.svg',['rbcl']],
+['polyA.svg',['rbcl']],
+['polypeptide-region.svg',['rbcl']],
+['primer-binding-site.svg',['rbcl']],
+['process.svg',['rbcl']],
+['promoter.svg',['rbcl']],
+['protease-site.svg',['rbcl']],
+['protein-stability-element.svg',['rbcl']],
+['protein.svg',['rbcl']],
+['replacement-glyph.svg',['rbcl']],
+['ribonuclease-site.svg',['rbcl']],
+['ribosome-entry-site.svg',['rbcl']],
+['rna-stability-element.svg',['rbcl']],
+['signature.svg',['rbcl']],
+['simple-chemical-circle.svg',['rbcl']],
+['simple-chemical-hexagon.svg',['rbcl']],
+['simple-chemical-pentagon.svg',['rbcl']],
+['simple-chemical-triangle.svg',['rbcl']],
+['specific-recombination-site.svg',['rbcl']],
+['ssNA.svg',['rbcl']],
+['stimulation.svg',['rbcl']],
+['terminator.svg',['rbcl']],
+['three-prime-overhang.svg',['rbcl']],
+['three-prime-sticky-restriction-site.svg',['rbcl']],
+['transcription-end.svg',['rbcl']],
+['translation-end.svg',['rbcl']],
+['unspecified-glyph.svg',['rbcl']],
+]);
 
 for (file in files){
 	var btn = document.createElement("button");
@@ -20,8 +93,10 @@ for (file in files){
 	btn.innerHTML = "<img src= 'Glyphs/"+files[file]+"'>";
 	btn.setAttribute("onclick","add_part(c, ctx, i, scale_f, h_offset, w_offset, 'Symbols/"+files[file]+"', )");
 	btn.setAttribute("draggable","true");
-	btn.setAttribute("ondragstart","drag(event)");
-document.getElementsByClassName("content")[0].appendChild(btn);
+	btn.setAttribute("ondragstart","drag(event);");
+	btn.setAttribute("oncontextmenu","openmodal("+"'"+files[file]+"'); return false;");
+	document.getElementsByClassName("content")[0].appendChild(btn);
+
 }
 
 class Node { // Linked list implementation
@@ -81,7 +156,7 @@ function update_display(canvas, context, const_width, scale, height_offset, widt
 function add_part(canvas, context, const_width, scale, height_offset, width_offset, path){ // add a symbol to the linked list of parts
 	var img = new Image(); // create an image object
 	img.src = path; // add its source (each button is unique)
-	img.onload = function(){ // DO canvasNOT TOUCH
+	img.onload = function(){ // DO NOT TOUCH
 		update_display(canvas, context, const_width, scale, height_offset, width_offset);
 	} // DO NOT TOUCH
 
@@ -182,6 +257,7 @@ function draw_circle(canvas, context, const_width, scale, height_offset){
 }
 
 
+/*
 function Download_Image(){ // DEPRECIATED download button implementation
 	var canvas = document.getElementById("main_canvas");
 	var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); //Convert image to 'octet-stream' (Just a download, really)
@@ -190,7 +266,6 @@ function Download_Image(){ // DEPRECIATED download button implementation
 	link.href = image;
 	link.click();
 }
-/*
 var coll = document.getElementsByClassName("collapsible");
 var x;
 for (x=0; x<coll.length; x++){
@@ -216,11 +291,8 @@ function drop(ev) {
 	ev.preventDefault();
 
 	mouseX = ev.clientX - c.offsetLeft;
-	console.log(construct_width, number_of_nodes, mouseX);
 	var position = Math.round(((mouseX - 10 - w_offset)/(construct_width+96))*number_of_nodes);
 	if (position > 0){
-		console.log(obj.src);
-		console.log(obj.src.lastIndexOf("/"));
 		insert_part(c, ctx, construct_width,2, h_offset, w_offset, "Symbols"+obj.src.substring(obj.src.lastIndexOf("/")), position);}
 	else {add_part(c, ctx, construct_width,2, h_offset, w_offset, "Symbols"+obj.src.substring(obj.src.lastIndexOf("/")), position);}
 }
@@ -232,9 +304,7 @@ ev.preventDefault();
 function insert_part(canvas, context, const_width, scale, height_offset, width_offset, path, position){ // add a symbol to the linked list of parts
 	var img = new Image(); // create an image object
 	img.src = path; // add its source (each button is unique)
-	console.log(img);
 	img.onload = function(){ // DO NOT TOUCH
-		console.log("beep");
 		update_display(canvas, context, const_width, scale, height_offset, width_offset);
 		active_node = new_node;
 	} // DO NOT TOUCH
@@ -268,4 +338,32 @@ function insert_part(canvas, context, const_width, scale, height_offset, width_o
 
 }
 
+var modal = document.getElementById("MyModal");
+function openmodal(identifier){
+	const body = document.getElementsByClassName("modal-body")[0];
+	while (body.hasChildNodes()){
+		body.removeChild(body.firstChild);}
+	modal.style.display = "block";
 
+	matches = hashtable.get(identifier);
+	for (element in matches){
+		let quick_add_btn = document.createElement("button");
+		quick_add_btn.type = "button";
+		quick_add_btn.innerHTML = matches[element];
+		quick_add_btn.setAttribute("onclick","quick_add('Symbols/"+identifier+"', "+ "'"+ matches[element]+ "')");
+	document.getElementsByClassName("modal-body")[0].appendChild(quick_add_btn);
+}
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }}
+
+function quick_add(path, text){
+add_part(c, ctx, construct_width, 2, h_offset, w_offset, path) // add a symbol to the linked list of parts
+	if (active_node != null){
+		active_node.text = text;
+		update_display(c, ctx, construct_width, 2, h_offset, w_offset);
+}
+	}
